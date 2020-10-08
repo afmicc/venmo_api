c51517fdf920dc409cbc67b8490db1a66db9cb99
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-10-08 17:01:05 -0300

    Add friendship model and seed data

---
+++ spec/factories/friendships.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  friend_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryBot.define do
+  factory :friendship do
+    user
+    friend
+  end
+end
