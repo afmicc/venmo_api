1c6278775c200027cfc9a07ff730b3f87f5956ef
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-10-09 12:49:32 -0300

    Add endpoint to display the user feed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      include Pagy::Backend
+
       def show; end
 
       def create
@@ -8,10 +10,20 @@ module Api
         render :show
       end
 
+      def feed
+        query = Event.where(user_id: current_user.contacts_for_feed).order(created_at: :desc)
+        pagy, @events = pagy(query, page: page)
+        @metadata = pagy_metadata(pagy)
+      end
+
       private
 
       def user_params
         params.require(:user).permit(:email, :name)
+      end
+
+      def page
+        params[:page] || 1
       end
     end
   end
